9f4fa01be3a9c9eca263b4a4728d22a7a2e15032
alfredo-neric <alfredo.neric@gmail.com>
2017-12-01 19:00:02 -0600

    diseño page

---
+++ footer.php
@@ -0,0 +1,29 @@
+				</div>
+			</ol>
+		</div>
+    <footer class="sticky-footer">
+		<div class="container">
+			<div class="text-center">
+				<small>Sistema de Inventario on-Line</small>
+			</div>
+		</div>
+    </footer>
+    <a class="scroll-to-top rounded" href="#page-top">
+		<i class="fa fa-angle-up"></i>
+    </a>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+	<!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+  </div>
+</body>
+
+</html>
--- home.php
+++ home.php
@@ -1,89 +1,13 @@
-<?php include('seguridad.php');?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<title>Inventario</title>
-	<!-- Bootstrap core CSS-->
-	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<!-- Custom fonts for this template-->
-	<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<!-- Custom styles for this template-->
-	<link href="css/sb-admin.css" rel="stylesheet">
-</head>
-
-<body class="fixed-nav sticky-footer bg-dark" id="page-top" oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
-	<!-- Navigation-->
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-		<a class="navbar-brand" href="home.php">Inventario</a>
-		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarResponsive">
-		<ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-			<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-				<a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-					<i class="fa fa-fw fa-wrench"></i>
-					<span class="nav-link-text">Catálogos</span>
-				</a>
-				<ul class="sidenav-second-level collapse" id="collapseComponents">
-					<li>
-						<a href="#">Catálogo 1</a>
-					</li>
-					<li>
-						<a href="#">Catálogo 2</a>
-					</li>
-				</ul>
-			</li>
-		</ul>
-		<ul class="navbar-nav sidenav-toggler">
-			<li class="nav-item">
-				<a class="nav-link text-center" id="sidenavToggler">
-					<i class="fa fa-fw fa-angle-left"></i>
-				</a>
-			</li>
-		</ul>
-		<ul class="navbar-nav ml-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="logout.php"><i class="fa fa-fw fa-sign-out"></i>Salir</a>
-			</li>
-		</ul>
-		</div>
-	</nav>
-	<div class="content-wrapper">
-		<div class="container-fluid">
-			<ol class="breadcrumb">
-				<div class="col-12 ">
-					<center>
-						<h1>Bienvenido</h1>
-						<h2>Sistema de Inventarios on-Line</h2>
-						<img src="img/140.png" class="img-fluid" width="65%" />
-					</center>
-				</div>
-			</ol>
-		</div>
-    <footer class="sticky-footer">
-		<div class="container">
-			<div class="text-center">
-				<small>Sistema de Inventario on-Line</small>
-			</div>
-		</div>
-    </footer>
-    <a class="scroll-to-top rounded" href="#page-top">
-		<i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin.min.js"></script>
-  </div>
-</body>
-
-</html>
+<?php 
+	include('seguridad.php');
+	include('header.php');
+?>
+<center>
+	<h1>Bienvenido</h1>
+	<h2>Sistema de Inventario on-Line</h2>
+	<img src="img/140.png" class="img-fluid" width="65%" />
+</center>
+<?php
+	include('footer.php');
+?>
+				
